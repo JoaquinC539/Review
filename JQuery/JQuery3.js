d98b877b76eea1ddff803e7bf2a1138ee5902eba
JoaquinC539 <cjoaquinangel@gmail.com>
2022-10-03 13:23:08 -0500

    Add new exercise

--- JQuery/JQuery3.js
+++ JQuery/JQuery3.js
@@ -3,4 +3,10 @@ function clean(){
 	var classClear=document.querySelectorAll('.exercise');
 	classClear.forEach(exercise=>{
 		exercise.remove();});}
-        
+
+
+   $("#addbtn").click(function(){ 
+    var buy=$("#tobuy").val();
+    $("#list").append("<ul>"+buy+"</ul>");
+
+   });
