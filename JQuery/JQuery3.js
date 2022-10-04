ef1bc4f9517656f3403eb0abdd01e5de9966414d
JoaquinC539 <cjoaquinangel@gmail.com>
2022-10-04 12:50:41 -0500

    End of new page

--- JQuery/JQuery3.js
+++ JQuery/JQuery3.js
@@ -10,3 +10,24 @@ function clean(){
     $("#list").append("<ul>"+buy+"</ul>");
 
    });
+
+   function RemoveOptions(){
+    var NewCiv=$("#newciv").val();
+    $("#selection")
+    .empty()
+    .append("<option selected='selected' value='test'>"+NewCiv+"</option>");
+   }
+
+   function underline(){
+    var text=$("#toUnderline").val().split(" ");
+    for(var i=0;i<text.length;i++){
+    var appendedtext=$("#content1").append("<span>"+text[i]+"</span>"+" ");
+    }
+    
+    appendedtext.addClass("underlined");    
+   }
+
+   function removeClass(){
+    $(".toRemove").removeClass();
+    
+   }
