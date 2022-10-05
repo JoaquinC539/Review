327127254cca68d17325083b7b9022afcb3a67ae
JoaquinC539 <cjoaquinangel@gmail.com>
2022-10-05 12:16:09 -0500

    End of page4

--- JQuery/JQuery4.js
+++ JQuery/JQuery4.js
@@ -3,3 +3,44 @@ function clean(){
 	var classClear=document.querySelectorAll('.exercise');
 	classClear.forEach(exercise=>{
 		exercise.remove();});}
+
+function removeCSS(){
+    $("#toRemoveCSS").css("background","none");
+}
+
+
+
+$("#toClick").mousedown(function(event){
+    switch (event.which) {
+        case 1:
+            $("#content1").append('Click izquierdo');
+            break;
+        case 2:
+            $("#content1").append('Boton medio click.');
+            break;
+        case 3:
+            $("#content1").append('Click derecho.');
+            break;
+        default:
+            $("#content1").append('Tienes un mouse extraño');
+    }
+});
+
+$(document).keypress(function(e){
+    if(e.which==13){
+        alert("Tecla enter pulsada");
+    }
+});
+
+$.fn.rowCount=function(){
+    return $('tr',$(this).find('tbody')).length;
+}
+
+$.fn.columnCount = function() {
+    return $('th', $(this).find('tbody')).length;
+}
+
+var rowctr=$("#table1").rowCount();
+var colctr=$("#table1").columnCount();
+
+$("#print").append("Filas: "+rowctr+" Columnas: "+colctr);
